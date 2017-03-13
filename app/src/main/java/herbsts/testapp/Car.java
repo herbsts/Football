e42e4032b7195c5a609932cb7044ca1ec16a9bc8
Stefan Herbst <herbsts@edu.htl-villach.at>
2017-03-13 14:55:43 +0100

    first test commit

---
+++ app/src/main/java/herbsts/testapp/Car.java
@@ -0,0 +1,46 @@
+package herbsts.testapp;
+
+/**
+ * Created by Stefan Herbst on 13.03.2017.
+ */
+
+public class Car {
+    private String name = null;
+    private int ps = -1;
+    private int constructed = -1;
+
+    public Car(String name, int ps, int constructed) {
+        this.name = name;
+        this.ps = ps;
+        this.constructed = constructed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPs() {
+        return ps;
+    }
+
+    public void setPs(int ps) {
+        this.ps = ps;
+    }
+
+    public int getConstructed() {
+        return constructed;
+    }
+
+    public void setConstructed(int constructed) {
+        this.constructed = constructed;
+    }
+
+    @Override
+    public String toString() {
+        return name+ps+constructed;
+    }
+}
