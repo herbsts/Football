5c97612456b3e4c2bbc8dbf6f67e43f1b65dff9a
Stefan Herbst <herbsts@edu.htl-villach.at>
2017-03-15 12:51:38 +0100

    comment in MainActivity.java updated

--- app/src/main/java/herbsts/soccer/MainActivity.java
+++ app/src/main/java/herbsts/soccer/MainActivity.java
@@ -2,9 +2,8 @@ package herbsts.soccer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Comment after commit
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
