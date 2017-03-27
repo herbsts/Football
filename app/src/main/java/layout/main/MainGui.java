4498de628263ee68bc9769e639b0053db3e88add
Armin <haracicarmin6@gmail.com>
2017-03-27 14:00:58 +0200

    maingui

---
+++ app/src/main/java/layout/main/MainGui.java
@@ -0,0 +1,15 @@
+package layout.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import herbsts.soccer.R;
+
+public class MainGui extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_gui);
+    }
+}
